60ea83812b8bb69dc3199e95031e7f402fb5f893
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-22 20:27:59 +0600

    Update 01. Create a structure named student. Store information for a students about the student's name, id, marks in physics, marks in math, marks in chemistry .  Add all those 3 courses marks. Print the student's name, id and  total marks in the output.c

--- 01. Create a structure named student. Store information for a students about the student's name, id, marks in physics, marks in math, marks in chemistry .  Add all those 3 courses marks. Print the student's name, id and  total marks in the output.c
+++ 01. Create a structure named student. Store information for a students about the student's name, id, marks in physics, marks in math, marks in chemistry .  Add all those 3 courses marks. Print the student's name, id and  total marks in the output.c
@@ -1,1 +1,22 @@
-
+#include <stdio.h>
+struct student
+{
+    char name[100];
+    int id;
+    float phy, che, mat;
+}
+s[5];
+int main() {
+    int i;
+    printf("Enter Name, Id, Physics, Chemistry, and Math Marks: \n");
+    for (i=0; i<5; i++)
+    {
+        scanf("%s %d %f %f %f", &s[i].name,&s[i].id,&s[i].phy,&s[i].che,&s[i].mat);
+    }
+    printf("Student Information:\n");
+ for (int i=0; i<5; i++)
+    {
+        printf("Name: %c \n Id: %d \n Total: %.2f", s[i].name, s[i].id, s[i].phy +  s[i].phy + s[i].phy);
+    }
+    return 0;
+}
