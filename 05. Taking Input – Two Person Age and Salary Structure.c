fe50667d5c86cb2042daf3651bbd24abeee321d7
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-24 20:45:36 +0600

    Create 05. Taking Input – Two Person Age and Salary Structure.c

---
+++ 05. Taking Input – Two Person Age and Salary Structure.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+   struct person { 
+   int age; 
+   float salary; }; 
+   int main() { 
+   struct person jakaria, jubayer; 
+   printf("Enter Jakaria's Age:  "); 
+   scanf("%d", &jakaria.age); 
+   printf("Enter Jakaria's Salary:  "); 
+   scanf("%f", &jakaria.salary); 
+   printf("Enter Jubayer's Age:  "); 
+   scanf("%d", &jubayer.age); 
+   printf("Enter Jubayer's Salary:  "); 
+   scanf("%f", &jubayer.salary); 
+   printf("Md Jakaria Nur\n"); 
+   printf("Age = %d Years\n", jakaria.age); 
+   printf("Salary = %.2f USD\n", jakaria.salary); 
+   printf("\nMd Jubayer Nur\n"); 
+   printf("Age = %d Years\n", jubayer.age); 
+   printf("Salary = %.2f USD\n", jubayer.salary); 
+   return 0 ; }
