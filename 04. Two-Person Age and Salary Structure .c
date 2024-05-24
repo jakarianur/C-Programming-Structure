4f5b03ff66d3e867687d9cf1e25c0ac59821bde5
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-24 20:44:50 +0600

    Create 04. Two-Person Age and Salary Structure .c

---
+++ 04. Two-Person Age and Salary Structure .c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+   struct person { 
+   int age; 
+   float salary;  }; 
+   int main() { 
+   struct person jakaria, jubayer; 
+   jakaria.age = 22; 
+   jakaria.salary = 3200.60; 
+   printf("Md Jakaria Nur\n"); 
+   printf("Age = %d Years\n", jakaria.age); 
+   printf("Salary = %.2f USD\n", jakaria.salary); 
+   jubayer.age = 24; 
+   jubayer.salary = 3400.60; 
+   printf("\nMd Jubayer Nur\n"); 
+   printf("Age = %d Years\n", jubayer.age); 
+   printf("Salary = %.2f USD\n", jubayer.salary); 
+    return 0 ; }
