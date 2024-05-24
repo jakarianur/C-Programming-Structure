f52f82d9f29d8b3c4c420293922a70363bf5a4db
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-24 23:55:53 +0600

    Update 02. Write a Code in C to Add two distances (in inch-feet) using structure.c

--- 02. Write a Code in C to Add two distances (in inch-feet) using structure.c
+++ 02. Write a Code in C to Add two distances (in inch-feet) using structure.c
@@ -7,13 +7,15 @@ struct Distance addDistances(struct Distance d1, struct Distance d2) {
     struct Distance result;
     result.inches = d1.inches + d2.inches;
     result.feet = d1.feet + d2.feet;
-    if (result.inches >= 12) {
+    if (result.inches >= 12)
+    {
         result.feet += result.inches / 12;
         result.inches = result.inches % 12;
     }
     return result;
 }
-int main() {
+int main()
+{
     struct Distance d1, d2, result;
     printf("Enter first distance (feet inches): ");
     scanf("%d %d", &d1.feet, &d1.inches);
